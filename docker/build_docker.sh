a91dff5816674a82066ccca82310910e941c9071
Michal Podhradsky <mpodhradsky@galois.com>
2023-10-26 14:47:41 -0700

    Update build script

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -3,7 +3,6 @@
 mkdir -p dist
 cd ..
 cp ./code/extensions/asmeta.validator/asmeta.validator/dist/AsmetaV.jar docker/dist/.
-cp ./code/extensions/asmeta.flattener/asmeta.flattener.cli/dist/AsmetaF.jar docker/dist/.
 cp ./code/extensions/asmetasmv/asmeta.smv/dist/AsmetaSMV.jar docker/dist/.
 cp ./code/extensions/asmeta.modeladvisor/asmeta.modeladvisor/dist/AsmetaMA.jar docker/dist/.
 cp ./code/extensions/asmeta.refprover/translatorToSMT/dist/AsmetaToSMT.jar docker/dist/.
@@ -12,6 +11,8 @@ cp ./code/core/asmeta.parser/asmeta.parser.cli/dist/AsmetaLc.jar docker/dist/.
 cp ./code/core/asmeta.simulator/asmeta.simulator.cli/dist/AsmetaS.jar docker/dist/.
 cp ./code/experimental/asmetal2cpp/asmetal2cpp_cli/dist/Asmeta2Cpp.jar docker/dist/.
 cd -
+# Kludge for keeping Asm2C CLI around
+cp ./Asmeta2C.jar ./dist/.
 
 sudo docker build -t podhrmic/asmeta:latest .
 sudo docker push podhrmic/asmeta:latest
